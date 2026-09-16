1d86c3a6b0507108ae11a41585f3d8fd0e113feb
Eucliwood26 <86105957+Eucliwood26@users.noreply.github.com>
2026-09-16 15:37:37 +0700

    change

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -15,6 +15,7 @@ static struct {
     const char *ssid;
     const char *password;
 } wifi_list[] __attribute__((unused)) = {
+    {.ssid = "xomtro", .password = "Hiep774271"},
     {.ssid = "Necromancer", .password = "DNam88/22"},
     {.ssid = "OTA", .password = "12345678"},
     {.ssid = "DAP", .password = "12345678"},
